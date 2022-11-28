597bd169cae7d7b0780a90e1e68e0b25b4791af6
Nguyễn Xuân Phúc <phucnx1909@gmail.com>
2022-11-28 23:51:50 +0700

    feature: router crud order

--- src/routes/order.router.js
+++ src/routes/order.router.js
@@ -1,18 +1,20 @@
 import express from "express";
 import {
-    add,
-    list,
-    read,
-    remove,
-    update
-} from "../controllers/order.controller";
+    orderController
+} from "../controllers";
+import validate from "../middlewares/validate";
+import {
+    orderValidation
+} from '../validations'
+
 
 const router = express.Router();
 
-router.get('/orders', list)
-router.get('/orders/:id', read)
-router.post('/orders', add)
-router.delete('/orders/:id', remove)
-router.patch('/orders/:id', update)
+router.get('/orders', orderController.getAll)
+router.get('/orders/:id', validate(orderValidation.getById), orderController.getById)
+router.post('/orders', validate(orderValidation.createOrder), orderController.create)
+router.delete('/orders/:id', validate(orderValidation.getById), orderController.removeById)
+router.patch('/orders/:id', validate(orderValidation.createOrder), validate(orderValidation.getById), orderController.updateById)
+
 
 export default router;
