cb192f37b18e9338744d9c9a1a8f88db3b34d611
Nguyễn Xuân Phúc <phucnx1909@gmail.com>
2022-11-28 19:52:01 +0700

    create-order-model

---
+++ src/models/order.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const orderSchema = mongoose.Schema({
+    date: {
+        type: Date
+    },
+    serviceType: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    user: {
+        type: mongoose.ObjectId,
+        ref: 'user',
+    },
+    cateStore: {
+        type: mongoose.ObjectId,
+        ref: 'cateStore',
+    },
+    cateService: {
+        type: mongoose.ObjectId,
+        ref: 'cateService',
+    }
+}, {
+    timestamps: true
+})
