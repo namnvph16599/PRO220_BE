f706dda77d9d15ccdb49ab06f0651cffb93bd36a
= <huynguyenz2002@gmail.com>
2023-03-08 08:35:58 +0700

    fix:authentication

--- src/routes/acount.router.js
+++ src/routes/acount.router.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { accountController, AuthController } from '../controllers';
+import { verifyAndAdminAuth } from '../middlewares/auth';
 import validate from '../middlewares/validate';
 import { accountValidation } from '../validations';
 
 const router = Router();
 
-router.get('/accounts', accountController.getAll);
+router.get('/accounts',verifyAndAdminAuth, accountController.getAll);
 router.get('/account/:id', validate(accountValidation.getById), accountController.getById);
 router.delete('/accounts/:id', validate(accountValidation.getById), accountController.removeById);
 router.put('/accounts/:id', validate(accountValidation.getById), accountController.updateById);
