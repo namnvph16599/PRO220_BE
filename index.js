0373ed3338459e9009bb46457503524637c0feea
= <huynguyenz2002@gmail.com>
2023-04-08 11:28:23 +0700

    feat:Discount

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ import routerRole from './src/routes/role.router';
 import routerPermission from './src/routes/permission.router';
 import routerPost from './src/routes/post.router';
 import routerService from './src/routes/service.router';
+import routeDiscount from './src/routes/discount.router';
 
 const app = express();
 // const app = express();
@@ -44,6 +45,7 @@ app.use('/api', routerRole);
 app.use('/api', routerPermission);
 app.use('/api', routerPost);
 app.use('/api', routerService);
+app.use('/api', routeDiscount);
 
 // parse urlencoded request body
 app.use(
