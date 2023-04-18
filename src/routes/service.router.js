e20735029f51532272f25599ab1a629d2609408a
NguyenXuanPhuc19 <nguyenxuanphuc749@gmail.com>
2023-04-18 08:55:25 +0700

    service

--- src/routes/service.router.js
+++ src/routes/service.router.js
@@ -7,4 +7,11 @@ const router = express.Router();
 router.post('/service', ServiceController.createService);
 router.get('/service', ServiceController.listService);
 
+router.get('/banner/:id', ServiceController.getByIdService);
+router.delete('/banner/:id', ServiceController.removeByIdService);
+router.patch(
+    '/banner/:id',
+    ServiceController.updateByIdService,
+);
+
 export default router;
